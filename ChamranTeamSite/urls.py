3671c362a32eaa3074799bdddbe20aa7976d3a17
Sepehr Metanat <sepehr.metanat@gmail.com>
2019-04-29 23:26:39 +0430

    project created

---
+++ ChamranTeamSite/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('expert/', include('expert.urls')),
+    path('researcher/', include('researcher.urls')),
+    path('industry/', include('industry.urls')),
+]
---
+++ chamran_admin/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChamranAdminConfig(AppConfig):
+    name = 'chamran_admin'
