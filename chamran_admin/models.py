7e2747e0476fa844b97ab173128c7ec043e858f7
Sepehr Metanat <sepehr.metanat@gmail.com>
2019-05-17 01:06:24 +0430

    admin database

--- chamran_admin/models.py
+++ chamran_admin/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    title = models.CharField(max_length=128, verbose_name="عنوان", default="بدون عنوان")
+    text = models.TextField(verbose_name="متن پیام")
+
+    MESSAGE_TYPES = [
+        (0, "اطلاعیه"),
+        (1, "اخطار"),
+        (2, "پیشنهاد"),
+    ]
+
+    type = models.IntegerField(default=0, choices=MESSAGE_TYPES, verbose_name="نوع")
+    attachment = models.FileField(upload_to=None, verbose_name="ضمیمه")
+    receiver = models.ManyToManyField(User, verbose_name="گیرندگان")
+
+    def __str__(self):
+        return self.title
+
+    def get_user_messages(self, user_id):
+        user_messages = Message.objects.filter(receiver=User.objects.get(id=user_id))
+        return user_messages
+
+
+class News(models.Model):
+    title = models.CharField(max_length=128, verbose_name="عنوان", default="بدون عنوان")
+    text = models.TextField(verbose_name="متن خبر")
+    picture = models.ImageField(upload_to=None, verbose_name="تصویر خبر")
+    attachment = models.FileField(upload_to=None, verbose_name="ضمیمه")
+    link = models.CharField(max_length=128, verbose_name="لینک خبر")
+    writer = models.CharField(max_length=32, verbose_name="نویسنده")
+    date_submitted = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return 'news/' + self.link
