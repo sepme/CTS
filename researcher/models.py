abcd4f59da32f3d39dc3c38539e8aa2086dcc840
a-jafarzadeh <a.jafarzadeh1998@gmail.com>
2019-05-17 17:51:56 +0430

    database developed

--- researcher/models.py
+++ researcher/models.py
@@ -1,15 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import datetime
+
 class Researcher(models.Model):
-    user          = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile       = models.OneToOneField("Profile", verbose_name=("مشخصات فردی"), on_delete=models.CASCADE)
-    membershipFee = models.OneToOneField("membershipFee", verbose_name=("حق عضویت"), on_delete=models.CASCADE)
-    points        = models.FloatField(blank=True ,verbose_name ='امتیاز')
-
+    user               =  models.OneToOneField(User, on_delete=models.CASCADE)
+    researcherProfile  =  models.OneToOneField("ResearcherProfile", verbose_name=("مشخصات فردی"), on_delete=models.CASCADE)
+    membership_fee     =  models.OneToOneField("Membership_fee", verbose_name=("حق عضویت"), on_delete=models.CASCADE)
+    status             =  models.OneToOneField("Status", on_delete=models.CASCADE)
+    points             =  models.FloatField(default=0 ,verbose_name ='امتیاز')
+    
     def __str__(self):
-        return self.profile.name
+        return self.researcherProfile.name
 
+class Status(models.Model):
+    STATUS =(
+        ('signed_up' ,"فرم های مورد نیاز تکمیل نشده است. "),
+        ('not_answered' ,"به سوال پژوهشی پاسخ نداده است."),
+        ('free' ,"فعال - بدون پروژه"),
+        ('waiting' ,"فعال - در حال انتظار پروژه"),
+        ('involved' ,"فعال - درگیر پروژه"),
+        ('inactivated' ,"غیر فعال - تویط مدیر سایت غیر فعال شده است."),
+    )
+    status              = models.CharField( max_length=15 ,choices=STATUS)
+    inactivate_duration = models.DateTimeField( auto_now=False, auto_now_add=False ,blank=True)
+
+    def is_inactivate(self):
+        return datetime.datetime.now() < self.inactivate_duration
 
 class MembershipFee(models.Model):
     fee    =  models.IntegerField(verbose_name = 'هزینه')
@@ -19,9 +36,10 @@ class MembershipFee(models.Model):
     def __str__(self):
         return str(self.fee)
 
-class Profile(models.Model):
-    name   =  models.CharField( max_length=300 ,verbose_name = "نام و نام خانوادگی")
-    major  =  models.CharField( max_length=300 ,verbose_name = "رشته تحصیلی")
+class ResearcherProfile(models.Model):
+    name       =  models.CharField( max_length=300 ,verbose_name = "نام و نام خانوادگی")
+    birth_year =  models.models.DateField( auto_now=False, auto_now_add=False ,verbose_name = "سال تولد")
+    major      =  models.CharField( max_length=300 ,verbose_name = "رشته تحصیلی")
     
     GARADE_CHOICE = (
         ('bs', 'کارشناسی'),
@@ -29,13 +47,13 @@ class Profile(models.Model):
         ('phd', 'دکتری'),
         ('proPhd', 'دکتری حرفه‌ای'),
         )
-    grade       =  models.CharField( max_length=6 ,choices = GARADE_CHOICE ,verbose_name = "آخرین مدرک تحصیلی")
-    university  =  models.CharField( max_length=300 ,verbose_name = "دانشگاه محل تحصیل")
-    entryYear   =  models.IntegerField(verbose_name = "سال ورود")
-    address     =  models.CharField( max_length=500 ,verbose_name = "آدرس محل سکونت")
-    homeNumber  =  models.CharField( max_length=50 ,verbose_name = "تلفن منزل")
-    phoneNumebr =  models.CharField( max_length=50 ,verbose_name = "تلفن همراه")
-    email       =  models.EmailField(max_length=254 ,verbose_name = "پست الکترونیکی")
+    grade        =  models.CharField( max_length=6 ,choices = GARADE_CHOICE ,verbose_name = "آخرین مدرک تحصیلی")
+    university   =  models.CharField( max_length=300 ,verbose_name = "دانشگاه محل تحصیل")
+    entry_year   =  models.IntegerField(verbose_name = "سال ورود")
+    address      =  models.CharField( max_length=500 ,verbose_name = "آدرس محل سکونت" ,blank=True)
+    home_number  =  models.CharField( max_length=50 ,verbose_name = "تلفن منزل")
+    phone_numebr =  models.CharField( max_length=50 ,verbose_name = "تلفن همراه",blank=True)
+    email        =  models.EmailField(max_length=254 ,verbose_name = "پست الکترونیکی")
 
     one   = 1
     two   = 2
@@ -50,36 +68,36 @@ class Profile(models.Model):
             (five  , '5'),
     )
 
-    teamWork        =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "روحیه کار تیمی")
-    creative        =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "تفکر خلاقانه")
-    interestInMajor =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "علاقه به رشته تحصیلی")
-    motivation      =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "انگیزه داشتن برای انجام پروژه")
-    Sacrifice       =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "تعهد داشتن و از خود گذشتگی")
-    diligence       =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "پشتکار")
-    interestInlearn =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "علاقه به یادگیری")
-    Timeliness      =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "وقت­شناسی")
-    dataCollection  =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "جمع­ آوری داده­ ها")
-    AwarenessOfPrinciples  = models.IntegerField(choices= INT_CHOICE ,verbose_name = "آگاهی از اصول انجام پروژه")
+    team_work                =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "روحیه کار تیمی")
+    creative                 =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "تفکر خلاقانه")
+    interest_in_major        =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "علاقه به رشته تحصیلی")
+    motivation               =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "انگیزه داشتن برای انجام پروژه")
+    sacrifice                =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "تعهد داشتن و از خود گذشتگی")
+    diligence                =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "پشتکار")
+    interest_in_learn        =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "علاقه به یادگیری")
+    timeliness               =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "وقت­شناسی")
+    data_collection          =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "جمع­ آوری داده­ ها")
+    awareness_of_principles  =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "آگاهی از اصول انجام پروژه")
 
-    Description     =  models.TextField()
+    Description              =  models.TextField()
 
     def __str__(self):
         return self.name + " profile"
 
 class ScientificHistory(models.Model):
-    profile = models.ForeignKey("Profile", verbose_name="سوابق علمی", on_delete=models.CASCADE)
+    researcherProfile = models.ForeignKey("ResearcherProfile", verbose_name="سوابق علمی", on_delete=models.CASCADE)
 
-    grade           =  models.CharField( max_length=300 ,verbose_name = "مقطع تحصیلی")
-    major           =  models.CharField( max_length=300 ,verbose_name = "رشته تحصیلی")
-    university      =  models.CharField( max_length=300 ,verbose_name = "دانشگاه")
-    place           =  models.CharField( max_length=300 ,verbose_name = "شهر محل تحصیل")
-    graduatedYear   =  models.IntegerField(verbose_name = "سال اخذ مدرک")
+    grade            =  models.CharField( max_length=300 ,verbose_name = "مقطع تحصیلی")
+    major            =  models.CharField( max_length=300 ,verbose_name = "رشته تحصیلی")
+    university       =  models.CharField( max_length=300 ,verbose_name = "دانشگاه")
+    place            =  models.CharField( max_length=300 ,verbose_name = "شهر محل تحصیل")
+    graduated_year   =  models.IntegerField(verbose_name = "سال اخذ مدرک")
     
     def __str__(self):
         return self.grade
 
 class Record(models.Model):
-    profile = models.ForeignKey("Profile", verbose_name="سوابق اجرایی", on_delete=models.CASCADE)
+    researcherProfile = models.ForeignKey("ResearcherProfile", verbose_name="سوابق اجرایی", on_delete=models.CASCADE)
 
     post   =  models.CharField( max_length=300 ,verbose_name = "سمت")
     start  =  models.DateField(auto_now=False, auto_now_add=False ,verbose_name = "از تاریخ")
@@ -91,7 +109,7 @@ class Record(models.Model):
         return self.post
 
 class ResearchActivities(models.Model):
-    profile = models.ForeignKey("Profile", verbose_name= "سوابق پژوهشی", on_delete=models.CASCADE)
+    researcherProfile = models.ForeignKey("ResearcherProfile", verbose_name= "سوابق پژوهشی", on_delete=models.CASCADE)
 
     title         =  models.CharField( max_length=300 ,verbose_name ="عنوان طرح پژوهشی")
     presenter     =  models.CharField( max_length=50 ,verbose_name ="نام مجری")
@@ -107,7 +125,7 @@ class ResearchActivities(models.Model):
         return self.title
 
 class History(models.Model):
-    profile     = models.ForeignKey("Profile", verbose_name=("تاریخچه"), on_delete=models.CASCADE)
+    researcherProfile     = models.ForeignKey("ResearcherProfile", verbose_name="تاریخچه", on_delete=models.CASCADE)
 
     title       = models.CharField( max_length=300,  verbose_name=("عنوان پروژه"))
     start       = models.DateField(auto_now=False, auto_now_add=False ,verbose_name = "تاریخ شروع")
@@ -116,16 +134,17 @@ class History(models.Model):
         ('completed' ,'completed'),
         ('stoped' ,'stoped'),
     )
-    status      = models.CharField( max_length=9 ,choices = STATUS_CHOICE ,verbose_name="وضعیت")
-    point       = models.FloatField(verbose_name='امتیاز')
-    income      = models.IntegerField(verbose_name= 'درآمد')
-    involveTech = models.CharField(max_length=500 ,verbose_name='تکنیک ها')
+    status       = models.CharField( max_length=9 ,choices = STATUS_CHOICE ,verbose_name="وضعیت")
+    point        = models.FloatField(verbose_name='امتیاز')
+    income       = models.IntegerField(verbose_name= 'درآمد')
+    involve_tech = models.CharField(max_length=500 ,verbose_name='تکنیک ها')
 
     def __str__(self):
-        return "history of " + self.profile.name 
+        return "history of " + self.researcher_profile.name 
     
 class Evaluation(models.Model):
     researcher = models.ForeignKey("Researcher", on_delete=models.CASCADE)
+    evaluator  = models.OneToOneField("expert.Expert", on_delete=models.CASCADE)
     
     zero  = 0
     one   = 1
@@ -141,44 +160,62 @@ class Evaluation(models.Model):
             (five  , '5'),
     )
 
-    timeAssign          =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "به چه میزان دانشجو مطابق زمان از پیش اعلام شده در هفته، برای کار زمان تخصیص داد؟ ")
-    flexibility         =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "میزان رضایت شما از انعطاف زمانبندی ارائه شده توسط دانشجو، چقدر است؟")
-    masteryTechniq      =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "دانشجو تسلط بر تکنیک های اعلام شده چه میزان بوده است؟ ")
-    verificationRecords =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "صحت سوابق و مهارتهای اعلام شده توسط دانشجو چقدر بوده است؟")
-    professional        =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "میزان رضایت شما از رفتار حرفه ای دانشجو، چه اندازه است؟")
-    cooperation         =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "دانشجو چه میزان همکاری سازنده با سایر اعضای تیم داشته است؟(کمک به ارتقای سطح دانش علمی و عملی گروه)")
-    satisfaction        =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "میزان رضایت شما از دانشجو در ازای ارائه نتایج عملکرد به شما چقدر بوده است ؟")
-    totalSatisfaction   =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "میزان رضایت کلی شما از دانشجو چه اندازه است؟")
-    nextCooperation     =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "چقدر تمایل دارید در همکاری های بعدی با دانشجو همکاری کنید؟")
-    chamranTeam         =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "میزان رضایت شما از عملکرد چمران تیم در این پروژه چقدر بوده است؟")
+    time_assign          =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "به چه میزان دانشجو مطابق زمان از پیش اعلام شده در هفته، برای کار زمان تخصیص داد؟ ")
+    flexibility          =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "میزان رضایت شما از انعطاف زمانبندی ارائه شده توسط دانشجو، چقدر است؟")
+    mastery_techniq      =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "دانشجو تسلط بر تکنیک های اعلام شده چه میزان بوده است؟ ")
+    verification_records =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "صحت سوابق و مهارتهای اعلام شده توسط دانشجو چقدر بوده است؟")
+    professional         =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "میزان رضایت شما از رفتار حرفه ای دانشجو، چه اندازه است؟")
+    cooperation          =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "دانشجو چه میزان همکاری سازنده با سایر اعضای تیم داشته است؟(کمک به ارتقای سطح دانش علمی و عملی گروه)")
+    satisfaction         =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "میزان رضایت شما از دانشجو در ازای ارائه نتایج عملکرد به شما چقدر بوده است ؟")
+    total_satisfaction   =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "میزان رضایت کلی شما از دانشجو چه اندازه است؟")
+    next_cooperation     =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "چقدر تمایل دارید در همکاری های بعدی با دانشجو همکاری کنید؟")
+    chamran_team         =  models.IntegerField(choices= INT_CHOICE ,verbose_name = "میزان رضایت شما از عملکرد چمران تیم در این پروژه چقدر بوده است؟")
 
     def avarage(self):
         sum = 0.0
-        sum = self.timeAssign + self.flexibility + self.masteryTechniq + self.verificationRecords
-        sum = sum + self.professional + self.cooperation + self.satisfaction + self.totalSatisfaction
-        sum = sum + self.nextCooperation + self.chamranTeam
+        sum = self.time_assign + self.flexibility + self.mastery_techniq + self.verification_records
+        sum = sum + self.professional + self.cooperation + self.satisfaction + self.total_satisfaction
+        sum = sum + self.next_cooperation + self.chamran_team
         ava = float(sum / 10)
         return ava
 
 class Technique(models.Model):
     TYPE = (
-        ('molecularBiology'    , 'Molecular Biology'),
+        ('molecular_biology'   , 'Molecular Biology'),
         ('immunology'          , 'Immunology'),
         ('imaging'             , 'Imaging'),
         ('histology'           , 'Histology'),
-        ('generalLab'          , 'General Lab'),
-        ('animalLab'           , 'Animal Lab'),
-        ('labSafety'           , 'Lab Safety'),
+        ('general_lab'         , 'General Lab'),
+        ('animal_lab'          , 'Animal Lab'),
+        ('lab_safety'          , 'Lab Safety'),
         ('biochemistry'        , 'Biochemistry'),
-        ('cellularBiology'     , 'Cellular Biology'),
-        ('researchMethodology' , 'Research Methodology'),
+        ('cellular_biology'    , 'Cellular Biology'),
+        ('research_methodology', 'Research Methodology'),
     )
 
-    techniqType  = models.CharField(max_length=100 ,choices = TYPE)
-    techniqTitle = models.CharField(max_length=300 )
+    techniq_type  = models.CharField( max_length=100 ,choices = TYPE)
+    techniq_title = models.CharField( max_length=300 )
+    tutorial_link = models.CharField( max_length=500 )
 
 class TechniqueInstance(models.Model):
-    researcher = models.ForeignKey("Researcher", verbose_name=_(""), on_delete=models.CASCADE)
+    researcher = models.ForeignKey("Researcher", on_delete=models.CASCADE)
     technique  = models.OneToOneField("Technique", verbose_name="مهارت", on_delete=models.CASCADE)
-    level      = models.CharField( max_length=100 ,verbose_name='سطح مهارت')
-    evaluator  = models.CharField( max_length=300 ,verbose_name='ارزیابی کننده')
+    TECH_GRADE = (
+        ('A' ,'به صورت عملی در پروژه انجام داده است.'),
+        ('B' ,'به صورت عملی در کارگاه آموزش دیده است.'),
+        ('C' ,'به صورت تئوری آموزش دیده است.'),
+    )
+    level      = models.CharField( max_length=1 ,choices=TECH_GRADE ,verbose_name='سطح مهارت' ,blank=True)
+    evaluator  = models.CharField( max_length=300 ,verbose_name='ارزیابی کننده',blank=True)
+
+    def is_validated(self):
+        if self.level == 'A' or self.level == 'B' or self.level == 'C':
+            return True
+        return False
+
+class RequestedProject(models.Model):
+    researcher          = models.ForeignKey("Researcher", on_delete=models.CASCADE)
+    project             = models.OneToOneField("Industry.Project", on_delete=models.CASCADE)
+    date_requested      = models.DateField(auto_now=False, auto_now_add=False ,verbose_name='تاریخ درخواست')
+    least_hours_offered = models.IntegerField(default=0 ,verbose_name='حداقل مدت زمانی پیشنهادی در هفته')
+    most_hours_offered  = models.IntegerField(default=0 ,verbose_name='حداکثر مدت زمانی پیشنهادی در هفته')
