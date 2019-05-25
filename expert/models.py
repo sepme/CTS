1364ef5113360874811da2a082c2778b4b23ea2a
3ep-one <44468135+3ep-one@users.noreply.github.com>
2019-05-25 13:18:32 +0430

    Update models.py

--- expert/models.py
+++ expert/models.py
@@ -2,86 +2,85 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Expert(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    expert_form = models.OneToOneField(ExpertForm, on_delete=models.CASCADE) 
-    date_register = models.DateField(auto_now=False, auto_now_add=False)
-    expert_project_history = models.ForeignKey(ExpertProjectHistory, on_delete=models.CASCADE)
-    expert_point = models.IntegerField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE ,verbose_name = "کاربر استاد")
+    expert_form = models.OneToOneField(ExpertForm, on_delete=models.CASCADE ,verbose_name = "فرم استاد") 
+    date_register = models.DateField(auto_now=False, auto_now_add=False ,verbose_name = "تاریخ ثبت نام")
+    expert_point = models.IntegerField(verbose_name = "امتیاز استاد")
     def __str__(self):
         return self.expert_form.expert_firstname+self.expert_form.expert_lastname
 
 class ExpertForm(models.Model): 
-    expert_firstname = models.CharField(max_length=None)
-    expert_lastname = models.CharField(max_length=None)
-    special_field = models.CharField(max_length=None)
-    national_code = models.IntegerField()
-    scientific_rank = models.CharField(max_length=None)
-    university = models.CharField(max_length=None)
-    home_adress = models.CharField(max_length=None)
-    phone_number = models.IntegerField()
-    mobile_phone =  models.IntegerField()
-    email_adress = models.EmailField(max_length=254)
-    scientific_record =  models.ForeignKey(ScientificRecord, on_delete=models.CASCADE)
-    executive_record =  models.ForeignKey(ExecutiveRecord, on_delete=models.CASCADE)
-    research_record =  models.ForeignKey(ResearchRecord, on_delete=models.CASCADE)
-    paper_record =  models.ForeignKey(PaperRecord, on_delete=models.CASCADE)
-    eq_tset = models.OneToOneField(EqTset, on_delete=models.CASCADE)
-    awards = models.CharField(max_length=None)
-    method_of_introduction = models.CharField(max_length=None)
-    posetive_feature_chamt =  models.CharField(max_length=None)
-    lab_equipment = models.CharField(max_length=None)
-    number_of_resercher = models.IntegerField()
-    has_indusryial_researech = models.BooleanField()
-    number_of_grants = models.IntegerField()
-    technique =  models.ForeignKey(researcher.Technique, on_delete=models.CASCADE)
-    languages = models.CharField(max_length=None)
+    expert_firstname = models.CharField(max_length=None,verbose_name = "")
+    expert_lastname = models.CharField(max_length=None,verbose_name = "")
+    special_field = models.CharField(max_length=None,verbose_name = "")
+    national_code = models.IntegerField(verbose_name = "")
+    scientific_rank = models.CharField(max_length=None,verbose_name = "")
+    university = models.CharField(max_length=None,verbose_name = "")
+    home_adress = models.CharField(max_length=None,verbose_name = "")
+    phone_number = models.IntegerField(verbose_name = "")
+    mobile_phone =  models.IntegerField(verbose_name = "")
+    email_adress = models.EmailField(max_length=254,verbose_name = "")
+    eq_tset = models.OneToOneField(EqTset, on_delete=models.CASCADE,verbose_name = "")
+    awards = models.CharField(max_length=None,verbose_name = "")
+    method_of_introduction = models.CharField(max_length=None,verbose_name = "")
+    posetive_feature_chamt =  models.CharField(max_length=None,verbose_name = "")
+    lab_equipment = models.CharField(max_length=None,verbose_name = "")
+    number_of_resercher = models.IntegerField(verbose_name = "")
+    has_indusryial_researech = models.BooleanField(verbose_name = "")
+    number_of_grants = models.IntegerField(verbose_name = "")
+    technique =  models.ManyToManyField(researcher.Technique,verbose_name = "")
+    languages = models.CharField(max_length=None,verbose_name = "")
 
 class ScientificRecord(models.Model):
-    degree = models.CharField(max_length=None)
-    major = models.CharField(max_length=None)
-    university = models.CharField(max_length=None)
-    city = models.CharField(max_length=None)
-    date_of_grauate = models.DateField(auto_now=False, auto_now_add=False)
-
+    degree = models.CharField(max_length=None,verbose_name = "")
+    major = models.CharField(max_length=None,verbose_name = "")
+    university = models.CharField(max_length=None,verbose_name = "")
+    city = models.CharField(max_length=None,verbose_name = "")
+    date_of_grauate = models.DateField(auto_now=False, auto_now_add=False,verbose_name = "")
+    expert_form =  models.ForeignKey(ExpertForm, on_delete=models.CASCADE,verbose_name = "فرم استاد")
+    
 class ExecutiveRecord(models.Model):
-    executive_post = models.CharField(max_length=None)
-    date_start_post = models.DateField(auto_now=False, auto_now_add=False)
-    date_end_post = models.DateField(auto_now=False, auto_now_add=False)
-    city =  models.CharField(max_length=None)
-    organization =  models.CharField(max_length=None)
+    executive_post = models.CharField(max_length=None,verbose_name = "")
+    date_start_post = models.DateField(auto_now=False, auto_now_add=False,verbose_name = "")
+    date_end_post = models.DateField(auto_now=False, auto_now_add=False,verbose_name = "")
+    city =  models.CharField(max_length=None,verbose_name = "")
+    organization =  models.CharField(max_length=None,verbose_name = "")
+    expert_form =  models.ForeignKey(ExpertForm, on_delete=models.CASCADE,verbose_name = "فرم استاد")
 
 class ResearchRecord(models.Model):
-    research_title = models.CharField(max_length=None)
-    researcher = models.CharField(max_length=None)
-    co_researcher = models.CharField(max_length=None)
-    status = models.CharField(max_length=None)
+    research_title = models.CharField(max_length=None,verbose_name = "")
+    researcher = models.CharField(max_length=None,verbose_name = "")
+    co_researcher = models.CharField(max_length=None,verbose_name = "")
+    status = models.CharField(max_length=None,verbose_name = "")
+    expert_form =  models.ForeignKey(ExpertForm, on_delete=models.CASCADE,verbose_name = "فرم استاد")
 
 class PaperRecord(models.Model):
-    research_title = models.CharField(max_length=None)
-    date_published = models.DateField(auto_now=False, auto_now_add=False)
-    publised_at = models.CharField(max_length=None)
-    impact_factor = models.FloatField()
-    citation = models.IntegerField()
-
+    research_title = models.CharField(max_length=None,verbose_name = "")
+    date_published = models.DateField(auto_now=False, auto_now_add=False,verbose_name = "")
+    publised_at = models.CharField(max_length=None,verbose_name = "")
+    impact_factor = models.FloatField(verbose_name = "")
+    citation = models.IntegerField(verbose_name = "")
+    expert_form =  models.ForeignKey(ExpertForm, on_delete=models.CASCADE,verbose_name = "فرم استاد")
+    
 class EqTset(models.Model):
-    team_work = models.IntegerField()
-    innovation = models.IntegerField()
-    devtion = models.IntegerField()
-    productive_research = models.IntegerField()
-    national_commitment = models.IntegerField()
-    collecting_information = models.IntegerField()
-    business_thinking = models.IntegerField()
-    risk_averse = models.IntegerField()
+    team_work = models.IntegerField(verbose_name = "")
+    innovation = models.IntegerField(verbose_name = "")
+    devtion = models.IntegerField(verbose_name = "")
+    productive_research = models.IntegerField(verbose_name = "")
+    national_commitment = models.IntegerField(verbose_name = "")
+    collecting_information = models.IntegerField(verbose_name = "")
+    business_thinking = models.IntegerField(verbose_name = "")
+    risk_averse = models.IntegerField(verbose_name = "")
 
 class Technique(models.Model):
-    technique_name = models.CharField(max_length=None)
-    ready_to_teach =  models.BooleanField()
-    skill_level = models.CharField(max_length=None)
+    technique_name = models.CharField(max_length=None,verbose_name = "")
+    ready_to_teach =  models.BooleanField(verbose_name = "")
+    skill_level = models.CharField(max_length=None,verbose_name = "")
 
 class ExpertProjectHistory(models.Model):
-    project_title_english = models.CharField(max_length=None)
-    key_words = models.CharField(max_length=None)
-    project_priority_level = models.FloatField()
+    project_title_english = models.CharField(max_length=None,verbose_name = "")
+    key_words = models.CharField(max_length=None,verbose_name = "")
+    project_priority_level = models.FloatField(verbose_name = "")
     project_start_date = models.DateField(auto_now=False, auto_now_add=False ,verbose_name = "تاریخ شروع")
     project_end_date = models.DateField(auto_now=False, auto_now_add=False ,verbose_name = "تاریخ پایان")
     STATUS_CHOICE = (
@@ -92,14 +91,14 @@ class ExpertProjectHistory(models.Model):
     project_point = models.FloatField(verbose_name='امتیاز')
     project_income = models.IntegerField(verbose_name= 'درآمد')
     project_involveTech = models.CharField(max_length=500 ,verbose_name='تکنیک ها')
-
+    expert = models.ForeignKey(Expert, on_delete=models.CASCADE ,verbose_name = "استاد")
     def __str__(self):
         return "history of " + self.profile.name 
     
     
 class IndustryEvaluation(models.Model):
-    expert = models.ForeignKey(Expert, on_delete=models.CASCADE)
-    industry  = models.OneToOneField(industry.Industry, on_delete=models.CASCADE)
+    expert = models.ForeignKey(Expert, on_delete=models.CASCADE,verbose_name = "")
+    industry  = models.OneToOneField(industry.Industry, on_delete=models.CASCADE,verbose_name = "")
     INT_CHOICE =(
             ( 0 , '0'),
             ( 1 , '1'),
@@ -136,8 +135,8 @@ class IndustryEvaluation(models.Model):
         return ava
     
 class ResearcherEvaluation(models.Model):
-    expert = models.ForeignKey(Expert, on_delete=models.CASCADE)
-    researcher  = models.OneToOneField(researcher.Researcher, on_delete=models.CASCADE)
+    expert = models.ForeignKey(Expert, on_delete=models.CASCADE,verbose_name = "")
+    researcher  = models.OneToOneField(researcher.Researcher, on_delete=models.CASCADE,verbose_name = "")
     INT_CHOICE =(
             ( 0 , '0'),
             ( 1 , '1'),
