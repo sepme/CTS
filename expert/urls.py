1549a7550bf3ee70a3629140a8f7542ad340dadb
hamidrezaRanjbarpour <53371031+hamidrezaRanjbarpour@users.noreply.github.com>
2019-08-22 19:35:50 +0430

    changed url to function view

--- expert/urls.py
+++ expert/urls.py
@@ -5,7 +5,7 @@ app_name = 'expert'
 
 urlpatterns = [
     path('', views.Index.as_view(), name="index"),
-    path('userInfo/', views.UserInfo.as_view(), name="userInfo"),
+    path('userInfo/', views.user_info, name="userInfo"),
     path('researcher/', views.ResearcherRequest.as_view(), name="researcherRequest"),
     path('messages/', views.Messages.as_view(), name="messages"),
 ]
