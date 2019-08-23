b08223f9c29af92feb9f4922185fd436bb580146
hamidrezaRanjbarpour <53371031+hamidrezaRanjbarpour@users.noreply.github.com>
2019-08-23 19:33:32 +0430

    index url changed to function based view

--- expert/urls.py
+++ expert/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'expert'
 
 urlpatterns = [
-    path('', views.Index.as_view(), name="index"),
-    path('userInfo/', views.user_info, name="userInfo"),
+    path('', views.index, name="index"),
+    path('userInfo/', views.UserInfo.as_view(), name="userInfo"),
     path('researcher/', views.ResearcherRequest.as_view(), name="researcherRequest"),
     path('messages/', views.Messages.as_view(), name="messages"),
-]   
+]
