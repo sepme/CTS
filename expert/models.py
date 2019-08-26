547293db898bba80e5f77928ecbc964c2ee8fdbf
hamidrezaRanjbarpour <53371031+hamidrezaRanjbarpour@users.noreply.github.com>
2019-08-27 00:42:45 +0430

    Unique user_id error fixed. expert user status setting handled.

--- expert/models.py
+++ expert/models.py
@@ -6,14 +6,14 @@ from django.shortcuts import reverse, HttpResponseRedirect
 
 class ExpertUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="کاربر استاد")
-    expert_point = models.IntegerField(verbose_name="امتیاز استاد", default=0.0, null=True, blank=True)
+    expert_point = models.IntegerField(verbose_name="امتیاز استاد", default=0.0)
     STATUS = (
         ('signed_up', "فرم های مورد نیاز تکمیل نشده است. "),
         ('free', "فعال - بدون پروژه"),
         ('involved', "فعال - درگیر پروژه"),
         ('inactivated', "غیر فعال - تویط مدیر سایت غیر فعال شده است."),
     )
-    status = models.CharField(max_length=15, choices=STATUS, null=True , blank=True)
+    status = models.CharField(max_length=15, choices=STATUS)
 
     def __str__(self):
         return self.user.get_username()
@@ -41,7 +41,7 @@ class EqTest(models.Model):
 
 
 class ExpertForm(models.Model):
-    expert_form = models.OneToOneField('expert.ExpertUser', on_delete=models.CASCADE, verbose_name="فرم استاد" , null=True, blank=True)
+    expert_user = models.OneToOneField('expert.ExpertUser', on_delete=models.CASCADE, verbose_name="فرم استاد" , null=True, blank=True)
     expert_firstname = models.CharField(max_length=32, verbose_name="نام")
     expert_lastname = models.CharField(max_length=32, verbose_name="نام خانوادگی")
     special_field = models.CharField(max_length=256, verbose_name="حوزه تخصصی")
--- expert/views.py
+++ expert/views.py
@@ -1,5 +1,5 @@
 from django.views import generic
-from django.shortcuts import render, HttpResponseRedirect, reverse, HttpResponse
+from django.shortcuts import render, HttpResponseRedirect, reverse, HttpResponse, get_object_or_404
 from .models import ExpertForm, EqTest, ExpertUser
 from .forms import InitialInfoForm
 from django.contrib.auth.decorators import login_required
@@ -23,12 +23,11 @@ class Messages(generic.TemplateView):
 
 @login_required(login_url='/login/')
 def index(request):
-    first_time = True
+    expert_user = get_object_or_404(ExpertUser, user=request.user)
     if request.method == 'POST':
         form = InitialInfoForm(request.POST or None)
         print(form.is_valid())
         if form.is_valid():
-            print(form.cleaned_data)
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
             special_field = form.cleaned_data['special_field']
@@ -41,18 +40,17 @@ def index(request):
             email = form.cleaned_data['email_address']
 
             expert_form = ExpertForm.objects.create(expert_firstname=first_name, expert_lastname=last_name, special_field=special_field, national_code=melli_code,
-                                                    scientific_rank=scientific_rank, university=university,phone_number=home_number,home_address=address,
+                                                    scientific_rank=scientific_rank, university=university, phone_number=home_number,home_address=address,
                                                     mobile_phone=phone_number, email_address=email)
-            expert_user = ExpertUser.objects.create(user=request.user)
-            expert_form.expert_form = expert_user
+            expert_form.expert_user = expert_user
+            expert_user.status = 'free'
+            expert_user.save()
             expert_form.save()
-            first_time = False
-            print(expert_form)
-            return render(request, 'expert/index.html', {'form': form, 'first_time': first_time})
+            return HttpResponseRedirect(reverse('expert:index'))
 
     else:
         form = InitialInfoForm()
-    return render(request, 'expert/index.html', {'form': form, 'first_time': first_time})
+    return render(request, 'expert/index.html', {'form': form, 'expert_user': expert_user})
 
 
 def test(request):
