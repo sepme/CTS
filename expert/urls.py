846f855274bebacdfcbfe93725e99d6941fe6c4d
hamidrezaRanjbarpour <53371031+hamidrezaRanjbarpour@users.noreply.github.com>
2019-08-29 01:30:13 +0430

    unnecessary url deleted

--- expert/urls.py
+++ expert/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('userInfo/', views.UserInfo.as_view(), name="userInfo"),
     path('researcher/', views.ResearcherRequest.as_view(), name="researcherRequest"),
     path('messages/', views.Messages.as_view(), name="messages"),
-    path('test/', views.test, name="test"),
     path('questions/', views.Questions.as_view(), name="questions"),
 
 ]
