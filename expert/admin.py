65eaf032a1e1799b5dfb0d72b549ad74416bf8b2
hamidrezaRanjbarpour <53371031+hamidrezaRanjbarpour@users.noreply.github.com>
2019-09-09 19:38:36 +0430

    photo url added to profile image

--- expert/admin.py
+++ expert/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from . import models
 
+
 @admin.register(models.ExpertUser)
 class ExpertAdmin(admin.ModelAdmin):
     pass
+
+
+admin.site.register(models.ExpertForm)
